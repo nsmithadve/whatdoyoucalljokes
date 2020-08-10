b4901d26308d9b602b15aafbe2383733da29340c
nsmithadve <54743243+nsmithadve@users.noreply.github.com>
2020-08-10 15:46:59 -0500

    Add files via upload

---
+++ js/quotes.js
@@ -0,0 +1,59 @@
+var quotes = [
+
+    {
+    quote: "Time is an illusion. Lunchtime doubly so.",
+    source: "Douglas Adams",
+    },
+    {
+    quote: "Time is an illusion. Lunchtime doubly so.",
+    source: "Douglas Adams",
+    },
+    {
+    quote: "Time is an illusion. Lunchtime doubly so.",
+    source: "Douglas Adams",
+    },
+    {
+    quote: "Time is an illusion. Lunchtime doubly so.",
+    source: "Douglas Adams",
+    },
+    {
+    quote: "Time is an illusion. Lunchtime doubly so.",
+    source: "Douglas Adams",
+    },
+    {
+    quote: "Time is an illusion. Lunchtime doubly so.",
+    source: "Douglas Adams",
+    },
+    {
+    quote: "Time is an illusion. Lunchtime doubly so.",
+    source: "Douglas Adams",
+    },
+    {
+    quote: "Time is an illusion. Lunchtime doubly so.",
+    source: "Douglas Adams",
+    },
+    {
+    quote: "Time is an illusion. Lunchtime doubly so.",
+    source: "Douglas Adams",
+    },
+    {
+    quote: "Time is an illusion. Lunchtime doubly so.",
+    source: "Douglas Adams",
+    },
+    {
+    quote: "Time is an illusion. Lunchtime doubly so.",
+    source: "Douglas Adams",
+    },
+    {
+    quote: "Time is an illusion. Lunchtime doubly so.",
+    source: "Douglas Adams",
+    },
+    {
+    quote: "Time is an illusion. Lunchtime doubly so.",
+    source: "Douglas Adams",
+    },
+    {
+    quote: "Time is an illusion. Lunchtime doubly so.",
+    source: "Douglas Adams",
+    },
+];
